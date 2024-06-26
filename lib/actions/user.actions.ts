eb7f0a254db941cf47455b25bd7ca8acf1da49ca
mstout60 <mstout@eprospector.com>
2024-06-26 13:50:52 -0500

    implement appwrite authorization for sign in an sign up

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,58 @@
+"use server";
+
+import { createAdminClient, createSessionClient } from "@/lib/appwrite";
+import { cookies } from "next/headers";
+import { ID } from "node-appwrite";
+import { parseStringify } from "@/lib/utils";
+
+export const signIn = async ({ email, password }: signInProps) => {
+    try {
+        // Mutation / Database / Fetch
+        const { account } = await createAdminClient();
+        const response = await account.createEmailPasswordSession(email, password);
+
+        return parseStringify(response);
+    } catch (error) {
+        console.error('Error', error)
+    }
+}
+
+export const signUp = async (userData: SignUpParams) => {
+    const { email, password, firstName, lastName } = userData;
+
+    try {
+        // Appwrite
+        // Mutation / Database / Fetch
+        const { account } = await createAdminClient();
+
+        const newUserAccount = await account.create(
+            ID.unique(),
+            email,
+            password,
+            `${firstName} ${lastName}`
+        );
+
+        const session = await account.createEmailPasswordSession(email, password);
+
+        cookies().set("appwrite-session", session.secret, {
+            path: "/",
+            httpOnly: true,
+            sameSite: "strict",
+            secure: true,
+        });
+
+        return parseStringify(newUserAccount);
+
+    } catch (error) {
+        console.error('Error', error)
+    }
+}
+
+export async function getLoggedInUser() {
+    try {
+        const { account } = await createSessionClient();
+        return await account.get();
+    } catch (error) {
+        return null;
+    }
+}
