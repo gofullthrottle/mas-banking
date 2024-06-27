4c7ab89536ac9832aa97f5cde1ba2503fcfb7b76
mstout60 <mstout@eprospector.com>
2024-06-27 10:26:22 -0500

    implement Sentry and finish bank card layout

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -14,6 +14,7 @@ import { cn } from "@/lib/utils"
 import Image from "next/image"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import Footer from "@/components/Footer";
 
 
 const MobileNav = ({ user }: MobileNavProps) => {
@@ -90,7 +91,7 @@ const MobileNav = ({ user }: MobileNavProps) => {
                             </nav>
                         </SheetClose>
 
-                        FOOTER
+                        <Footer user={user} type="mobile" />
                     </div>
                 </SheetContent>
             </Sheet>
